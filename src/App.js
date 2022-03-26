c263aaa59a2761081e350bff17a1d20c71ae478a
timicodes21 <timileyinbabalola42@gmail.com>
2022-03-26 18:40:55 +0100

    testimonial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import TestimonialPage from "./components/testimonialPage";
-import { HashRouter, Routes, Route } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Story from "./components/story";
-
 
 function App() {
   return (
     <HashRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<TestimonialPage />} />
+          <Route exact path="/" element={<TestimonialPage />} />
           <Route path="/story" element={<Story />} />
         </Routes>
       </div>
